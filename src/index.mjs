455bfdc6cb3adbd5c1097f77d5f763d23014f48f
James Ross <james@jross.me>
2021-08-09 23:27:52 +0100

    refactor: switch to `pipeToWebWritable`
    This does not work yet unfortunately

--- src/index.mjs
+++ src/index.mjs
@@ -1,27 +1,13 @@
 import {createSSRApp} from 'vue';
-import {renderToWebStream} from '@vue/server-renderer';
-import {ReadableStream as polyReadableStream} from 'web-streams-polyfill/dist/ponyfill.es2018.mjs';
+import {pipeToWebWritable} from '@vue/server-renderer';
 
 import appVue from './app.vue';
-
-async function pipeReaderToWriter(reader, writer){
-	const encoder = new TextEncoder();
-	for(;;){
-		const {value, done} = await reader.read();
-		await writer.write(encoder.encode(value));
-		if(done){
-			break;
-		}
-	}
-	writer.close();
-}
 
 // eslint-disable-next-line no-unused-vars
 async function handleRequest(request, env){
 	const app = createSSRApp(appVue);
 	const {readable, writable} = new TransformStream();
-	const render = renderToWebStream(app, {}, polyReadableStream);
-	pipeReaderToWriter(render.getReader(), writable.getWriter());
+	pipeToWebWritable(app, {}, writable);
 	return new Response(readable);
 }
 
