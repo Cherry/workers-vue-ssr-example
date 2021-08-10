f47ce1ca445f114743af70465b0f3328a4c8d590
James Ross <james@jross.me>
2021-08-10 19:51:53 +0100

    fix: use manual `renderToSimpleStream` call, until `pipeToWebWritable` fix is released

--- src/index.mjs
+++ src/index.mjs
@@ -1,7 +1,26 @@
 import {createSSRApp} from 'vue';
-import {pipeToWebWritable} from '@vue/server-renderer';
+import {renderToSimpleStream} from '@vue/server-renderer';
 
 import appVue from './app.vue';
+
+const pipeToWebWritable = function(input, context, writable){
+	const writer = writable.getWriter();
+	const encoder = new TextEncoder();
+
+	renderToSimpleStream(input, context, {
+		async push(content){
+			if(content !== null){
+				return writer.write(encoder.encode(content));
+			}
+			return writer.close();
+		},
+		destroy(err){
+			// TODO better error handling?
+			console.log(err);
+			writer.close();
+		},
+	});
+};
 
 // eslint-disable-next-line no-unused-vars
 async function handleRequest(request, env){
