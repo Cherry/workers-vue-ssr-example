0b2d4db98fbc5da18c5c72b40b5aab88a6a94e5d
James Ross <james@jross.me>
2021-08-11 20:22:45 +0100

    fix: use `pipeToWebWritable` from Vue 3.2.2

--- src/index.mjs
+++ src/index.mjs
@@ -1,26 +1,7 @@
 import {createSSRApp} from 'vue';
-import {renderToSimpleStream} from '@vue/server-renderer';
+import {pipeToWebWritable} from '@vue/server-renderer';
 
 import appVue from './app.vue';
-
-const pipeToWebWritable = function(input, context, writable){
-	const writer = writable.getWriter();
-	const encoder = new TextEncoder();
-
-	renderToSimpleStream(input, context, {
-		async push(content){
-			if(content !== null){
-				return writer.write(encoder.encode(content));
-			}
-			return writer.close();
-		},
-		destroy(err){
-			// TODO better error handling?
-			console.log(err);
-			writer.close();
-		},
-	});
-};
 
 // eslint-disable-next-line no-unused-vars
 async function handleRequest(request, env){
